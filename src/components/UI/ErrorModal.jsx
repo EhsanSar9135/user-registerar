c058a6d86222b2e09454377d0aea8b4f6b283ead
ehsan saraee <ehsansaraee6570@yahoo.com>
2021-08-09 02:43:02 +0430

    Managing the Error via State

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Button from "./Button";
 import Card from "./Card";
 
-const ErrorModal = ({ title, message }) => {
+const ErrorModal = ({ title, message, errorHandler }) => {
    return (
       <>
-         <div className="backdrop" />
+         <div className="backdrop" onClick={errorHandler} />
          <Card className="modal">
             <header className="header">
                <h2>{title}</h2>
             </header>
             <section className="content">{message}</section>
             <footer className="actions">
-               <Button>Okay</Button>
+               <Button onClick={errorHandler}>Okay</Button>
             </footer>
          </Card>
       </>
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -4,17 +4,28 @@ import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = ({ onAddUser }) => {
+   // States
    const [enteredUsername, setEnteredUsername] = useState("");
    const [enteredAge, setEnteredAge] = useState("");
+   const [error, setError] = useState();
+   // Event Handlers
    const addUserHandler = (event) => {
       event.preventDefault();
       if (
          enteredUsername.trim().length === 0 ||
          enteredAge.trim().length === 0
       ) {
+         setError({
+            title: "Invalid input",
+            message: "Please enter a valid name and age (non-empty values)",
+         });
          return;
       }
       if (+enteredAge < 1) {
+         setError({
+            title: "Invalid age",
+            message: "Please enter a valid age (> 0)",
+         });
          return;
       }
       onAddUser(enteredUsername, enteredAge);
@@ -27,12 +38,18 @@ const AddUser = ({ onAddUser }) => {
    const ageChangeHandler = (event) => {
       setEnteredAge(event.target.value);
    };
+   const errorHandler = () => {
+      setError(null);
+   };
    return (
       <>
-         <ErrorModal
-            title="An error occurred!"
-            message="Something went wrong!"
-         />
+         {error && (
+            <ErrorModal
+               errorHandler={errorHandler}
+               title={error.title}
+               message={error.message}
+            />
+         )}
          <Card className="input">
             <form onSubmit={addUserHandler}>
                <label htmlFor="username">username</label>
