a8acb72ef3e56dcae0bffce1af7b81726e67f7d1
ehsan saraee <ehsansaraee6570@yahoo.com>
2021-08-09 16:02:37 +0430

    Optimize App with Fragments & Portals

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,20 +1,41 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 
-const ErrorModal = ({ title, message, errorHandler }) => {
+const Backdrop = ({ errorHandler }) => {
+   return <div className="backdrop" onClick={errorHandler} />;
+};
+
+const ModalOverlay = ({ title, message, errorHandler }) => {
+   return (
+      <Card className="modal">
+         <header className="header">
+            <h2>{title}</h2>
+         </header>
+         <section className="content">{message}</section>
+         <footer className="actions">
+            <Button onClick={errorHandler}>Okay</Button>
+         </footer>
+      </Card>
+   );
+};
+
+const ErrorModal = ({ errorHandler, title, message }) => {
    return (
       <>
-         <div className="backdrop" onClick={errorHandler} />
-         <Card className="modal">
-            <header className="header">
-               <h2>{title}</h2>
-            </header>
-            <section className="content">{message}</section>
-            <footer className="actions">
-               <Button onClick={errorHandler}>Okay</Button>
-            </footer>
-         </Card>
+         {ReactDOM.createPortal(
+            <Backdrop errorHandler={errorHandler} />,
+            document.getElementById("backdrop-root")
+         )}
+         {ReactDOM.createPortal(
+            <ModalOverlay
+               title={title}
+               message={message}
+               errorHandler={errorHandler}
+            />,
+            document.getElementById("overlay-root")
+         )}
       </>
    );
 };
